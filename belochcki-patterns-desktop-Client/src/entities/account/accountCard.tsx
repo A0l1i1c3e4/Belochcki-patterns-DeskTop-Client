7713a9b237a0595c1f81188ee4d0a6f67f337c64
A0l1i1c3e4 <vas120704@gmail.com>
2026-03-22 18:17:54 +0700

    Close debit

--- belochcki-patterns-desktop-Client/src/entities/account/accountCard.tsx
+++ belochcki-patterns-desktop-Client/src/entities/account/accountCard.tsx
@@ -19,6 +19,7 @@ export const AccountCard = ({ account: account }: Props) => {
       sx={{ cursor: "pointer" }}>
       <CardContent>
         <Typography variant="h6">{account.name}</Typography>
+        <Typography variant="h6">{account.id}</Typography>
         <Typography variant="h6">{account.balance}</Typography>
         <Typography variant="h6">{account.CurrencyCode}</Typography>
         <Typography variant="h6">{account.status}</Typography>
@@ -43,11 +44,12 @@ export const CreditAccountCard = ({ account: account }: Props) => {
       sx={{ cursor: "pointer" }}>
       <CardContent sx={{ p: 4, width: "100%", display: "flex",  flexDirection: "column" }}>
         <Typography variant="h6">{account.name}</Typography>
+        <Typography variant="h6">{account.id}</Typography>
         <Typography variant="h6">{account.balance}</Typography>
         <Typography variant="h6">{account.CurrencyCode}</Typography>
         <Typography variant="h6">{account.status}</Typography>
         <Typography variant="caption" display="block" sx={{ mt: 2 }}>
-          {new Date(account.createdTime + account.createdDate).toLocaleString()}
+          {`${account.createdDate} ${account.createdTime}`}
         </Typography>
       </CardContent>
     </Card>
--- belochcki-patterns-desktop-Client/src/pages/accountsPage/accountsPage.tsx
+++ belochcki-patterns-desktop-Client/src/pages/accountsPage/accountsPage.tsx
@@ -17,7 +17,7 @@ export const AccountsPage = () => {
   
   const loadDebitAccounts = async () => {
     try {
-      localStorage.setItem("accessToken", "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjOGQ0MGVlYi02ZWY2LTQ2MDQtOTk0OC0yZmVjMWMyMTgwYjkiLCJzY29wZSI6IkVNUExPWUVFIENMSUVOVCIsImxvZ2luIjoic3RyaW5nIiwiaWF0IjoxNzc0MTczNjY5LCJleHAiOjE3NzQxNzcyNjl9._i0chF_Ms7OGu-lm3XiezKss5QO12tbNo_1-W7_wUjk");
+      localStorage.setItem("accessToken", "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjOGQ0MGVlYi02ZWY2LTQ2MDQtOTk0OC0yZmVjMWMyMTgwYjkiLCJzY29wZSI6IkVNUExPWUVFIENMSUVOVCIsImxvZ2luIjoic3RyaW5nIiwiaWF0IjoxNzc0MTc3NDA2LCJleHAiOjE3NzQxODEwMDZ9.YyUxIvVmNG7on7zGChNK0RNGt72BG_nTeji1sT73tJI");
       localStorage.setItem("clientID", "c8d40eeb-6ef6-4604-9948-2fec1c2180b9");
       const data: AccountsResponse = await fetchDebitAccounts(page, pageSize);
 
